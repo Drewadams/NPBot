970616c78606ef060a460ac9280497ac9e307462
Drew Adams <drewadams044@gmail.com>
2018-12-23 14:42:15 -0500

    Added streamer variable so live role only applies there.

--- index.js
+++ index.js
@@ -32,22 +32,39 @@ client.on('guildMemberAdd', member => {
 client.on('presenceUpdate', (oldMember, newMember) => {
   // Checks for any game activity
   if (newMember.presence.game != null) {
+
+    // Variables
+    let liveRole = newMember.guild.roles.find(role => role.name === "Now Live");
+    let streamerRole = newMember.guild.roles.find(role => role.name === "NP Streamers/Content Creators");
+
     // If that person is streaming
     if (newMember.presence.game.streaming === true) {
-      console.log(`${newMember.displayName} is streaming!`);
-      let role = newMember.guild.roles.find(role => role.name === "Now Live");
-      newMember.addRole(role);
-      newMember.guild.channels.find(channel => channel.name === "self-advertising").send(`${newMember.displayName} is streaming! Come check it out at ${newMember.presence.game.url} !`)
 
-    } else {
-      let role = newMember.guild.roles.find(role => role.name === "Now Live");
-      console.log('got to the else statement');
-      if (newMember.highestRole === role) {
-        console.log('got to the if statement inside else');
-        let role = newMember.guild.roles.find(role => role.name === "Now Live");
-        console.log(role.name);
-        newMember.removeRole(role);
+      // Announces in self-advertising
+      console.log(`${newMember.displayName} is streaming!`);
+      newMember.guild.channels.find(channel => channel.name === "self-advertising").send(`${newMember.displayName} is streaming ${newMember.presence.game.name}! Come check it out: ${newMember.presence.game.url}`)
+
+      // If part of stream team.
+      if (newMember.highestRole === streamerRole) {
+        // Add role of Now Live.
+        console.log(`Giving Now Live role to ${newMember.displayName}.`);
+        newMember.addRole(liveRole);
       };
+
+      // removes Now Live when stream ends.
+    } else {
+      console.log('Presence update either: ended stream or wasn\'t streaming');
+      if (newMember.highestRole === liveRole) {
+        console.log(`removing ${liveRole.name}`);
+        newMember.removeRole(liveRole);
+      };
+    };
+    // If no game activity shown, still removes Now Live
+  } else {
+    let liveRole = newMember.guild.roles.find(role => role.name === "Now Live");
+    if (newMember.highestRole === liveRole) {
+      console.log(`Removing ${liveRole.name} from ${newMember.displayName} without game activity.`);
+      newMember.removeRole(liveRole);
     };
   };
 });
