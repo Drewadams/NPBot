b14081aaec02e773098196f6d740eafa534e015e
Drew Adams <drewadams044@gmail.com>
2018-12-23 19:32:06 -0500

    Updated NP emoji to not use special character.

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ client.on('guildMemberAdd', member => {
 
   // Welcome message
   // emojis
-  const np1 = client.emojis.find(emoji => emoji.name === "NP~1");
+  const npg = client.emojis.find(emoji => emoji.name === "NPG");
   const tripz = client.emojis.find(emoji => emoji.name === "TRIPZ");
   const nathan = client.emojis.find(emoji => emoji.name === "NATHAN");
   const daxedy = client.emojis.find(emoji => emoji.name === "Daxedy");
 
-  member.guild.channels.find(channel => channel.name === "welcome").send(`Hey ${member.user}, welcome to No Problem Gaming Public ${np1}! If you are here for a tryout please contact Tripz ${tripz} for Rust or Nathan ${nathan} and Daxedy ${daxedy} for siege, and please go to the <#511739375764111381> channel to receive a role, visit <#415684176461824000> for more info.`);
+  member.guild.channels.find(channel => channel.name === "welcome").send(`Hey ${member.user}, welcome to No Problem Gaming Public ${npg}! If you are here for a tryout please contact Tripz ${tripz} for Rust or Nathan ${nathan} and Daxedy ${daxedy} for siege, and please go to the <#511739375764111381> channel to receive a role, visit <#415684176461824000> for more info.`);
 
   // Send the message to DM:
   member.user.send(`Welcome to the NP Public server, ${member}!`);
