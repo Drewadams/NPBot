a0f8e635a4cc63d624d38eddb05642ceff916e33
Drew Adams <drewadams044@gmail.com>
2018-12-27 11:19:35 -0500

    Fixing last push

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
       // Announces in self-advertising
       if (oldMember.presence.game === null || newMember.presence.game.streaming != oldMember.presence.game.streaming) {
 
-        if (newMember.id === '246449524132937730') {
+        if (newMember.id != '246449524132937730') {
           // Only sends the message to people WHO DON'T SPAM THE SHIT
           console.log(`${newMember.displayName} is streaming!`);
           newMember.guild.channels.find(channel => channel.name === "self_advertisement").send(`${newMember.displayName} is streaming ${newMember.presence.game.name}! Come check it out: ${newMember.presence.game.url}`);
